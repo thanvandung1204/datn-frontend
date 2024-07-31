dd0be997b730e854708e2e065dd8f13c72ff4c6a
quanlmph18475 <quanlmph18475@fpt.edu.vn>
2024-07-31 22:13:29 +0700

    update-code

--- src/components/My-OrderDetail/index.tsx
+++ src/components/My-OrderDetail/index.tsx
@@ -1,5 +1,6 @@
 import { useParams, useNavigate } from 'react-router-dom'
 import { Steps } from 'antd'
+import { useGetOrderByidQuery } from '../../store/slices/order'
 import Loader from '../Loader'
 import { Divider } from 'antd'
 import { AiFillCreditCard, AiOutlineArrowLeft } from 'react-icons/ai'
@@ -12,6 +13,15 @@ import formatDate from '../../utils/formatDate'
 const MyOrderDetail = () => {
   const { id } = useParams()
   const navigate = useNavigate()
+  const { data: orderData, isError } = useGetOrderByidQuery(id as string)
+
+  const totalPrice = orderData?.order?.items.reduce(
+    (accumulator, item) =>
+      accumulator +
+      item.price * item?.quantity +
+      item?.toppings.reduce((acc: number, topping: ITopping) => acc + topping.price, 0),
+    0
+  )
 
   const items = [
     {
@@ -30,6 +40,10 @@ const MyOrderDetail = () => {
       title: 'Hoàn Thành'
     }
   ]
+  const currentStatus = items.find((item) => item.name === orderData?.order?.status)
+  if (isError) {
+    navigate(-1)
+  }
 
   return (
     <>
@@ -43,14 +57,28 @@ const MyOrderDetail = () => {
             </span>
           </div>
           <div className='uppercase flex items-center gap-x-3 text-sm'>
-            <span>Mã đơn hàng: 1</span>
+            <span>Mã đơn hàng: {orderData?.order?._id}</span>
             <span>|</span>
+
+            {orderData?.order?.status === 'canceled' ? (
+              <span className='text-[#EE4D2D]'>Đơn hàng đã được hủy</span>
+            ) : (
+              <span className='text-[#EE4D2D]'>Đơn hàng {currentStatus?.title}</span>
+            )}
           </div>
         </div>
         <Divider />
         <div className='order-status-step'>
           <div className='mb-10'>
             <h2 className='mb-5 text-xl text-[#866312]'>Trạng thái đơn hàng</h2>
+            {orderData?.order?.status === 'canceled' ? (
+              <div className='flex flex-col justify-center items-center bg-[#fffcf5] py-6 '>
+                <span className='text-[20px] text-[#ee4d2d]'>Đã hủy đơn hàng</span>
+                <span className='text-sm'>Lý do: {orderData?.order?.reasonCancelOrder}</span>
+              </div>
+            ) : (
+              <Steps labelPlacement='vertical' current={currentStatus?.index} items={items} />
+            )}
           </div>
         </div>
         <div className='address my-10'>
@@ -58,12 +86,19 @@ const MyOrderDetail = () => {
           <div className='bg_image'></div>
           <div className='py-5'>
             <div className='info flex flex-col'>
-              <span className='mb-2'>Tên người nhận: user</span>
-              <span className='text-[12px] text-[#0000008a]'>SĐT:00000</span>
-              <span className='text-[12px] text-[#0000008a]'>Địa chỉ: My Dinh Ha Noi</span>
-
-              <span className='text-[12px] text-[#0000008a]'>Ghi chú: 1</span>
-              <span className='text-[12px] text-[#0000008a]'>Thời gian đặt hàng: 123</span>
+              <span className='mb-2'>Tên người nhận: {orderData?.order?.inforOrderShipping?.name}</span>
+              <span className='text-[12px] text-[#0000008a]'>SĐT: {orderData?.order?.inforOrderShipping?.phone}</span>
+              <span className='text-[12px] text-[#0000008a]'>
+                Địa chỉ: {orderData?.order?.inforOrderShipping?.address}
+              </span>
+              {orderData?.order?.inforOrderShipping?.noteShipping?.trim() && (
+                <span className='text-[12px] text-[#0000008a]'>
+                  Ghi chú: {orderData?.order?.inforOrderShipping?.noteShipping}
+                </span>
+              )}
+              <span className='text-[12px] text-[#0000008a]'>
+                Thời gian đặt hàng: {orderData?.order?.createdAt && formatDate(orderData?.order?.createdAt)}
+              </span>
             </div>
           </div>
           <div className='bg_image'></div>
@@ -71,28 +106,48 @@ const MyOrderDetail = () => {
         <div className='content'>
           <h2 className='mb-4 text-xl text-[#866312]'>Sản phẩm đã đặt</h2>
           <div className='list-items'>
-            <div className='item flex items-center gap-x-3 mb-10 shadow-md p-2 rounded'>
-              <div className='left flex gap-x-3 flex-1'>
-                <div className='min-w-max'>
-                  <img src={'../../../public/icon-glass-tea.png'} alt='' className='w-full h-full object-cover' />
-                </div>
-                <div>
-                  <h4 className='title mb-2 text-[#866312] text-sm'>Tra sua</h4>
-                  <div className='flex flex-col gap-y-1'>
-                    <span className='text-[#866312] text-sm'>Size: S</span>
+            {orderData &&
+              orderData?.order?.items.length > 0 &&
+              orderData?.order?.items?.map((item, index) => (
+                <div key={index} className='item flex items-center gap-x-3 mb-10 shadow-md p-2 rounded'>
+                  <div className='left flex gap-x-3 flex-1'>
+                    <div className='min-w-max'>
+                      <img src={item?.image} alt='' className='w-[100px] h-[100px] object-cover' />
+                    </div>
+                    <div>
+                      <h4 className='title mb-2 text-[#866312] text-sm'>{item?.product.name}</h4>
+                      <div className='flex flex-col gap-y-1'>
+                        <span className='text-[#866312] text-sm'>Size: {item?.size?.name}</span>
+                        {item && item?.toppings.length > 0 && (
+                          <span className='text-sm text-[#866312]'>
+                            Toppings:{' '}
+                            {item?.toppings?.map((topping: ITopping) =>
+                              item.toppings[item.toppings.length - 1].name === topping.name
+                                ? `${topping.name}(${formatCurrency(topping.price)}).`
+                                : `${topping.name}(${formatCurrency(topping.price)}), `
+                            )}
+                          </span>
+                        )}
 
-                    <span className='text-sm text-[#866312]'>Toppings: </span>
-
-                    <span className='quantity text-[12px]'>x</span>
+                        <span className='quantity text-[12px]'>x{item?.quantity}</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='right'>
+                    <div className='price flex flex-col items-end'>
+                      <span className='text-[#866312] ml-2'>
+                        {formatCurrency(item?.price)} x {item?.quantity} ={' '}
+                        {formatCurrency(item?.price * item?.quantity)}
+                      </span>
+                      {item.toppings?.map((topping: ITopping) => (
+                        <span key={topping._id} className='text-[#866312] ml-2'>
+                          {formatCurrency(topping.price)}
+                        </span>
+                      ))}
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className='right'>
-                <div className='price flex flex-col items-end'>
-                  <span className='text-[#866312] ml-2'></span>
-                </div>
-              </div>
-            </div>
+              ))}
 
             {/* <div className='item flex items-center gap-x-3  shadow-md px-2 rounded'>
               <div className='left flex gap-x-3 flex-1'>
@@ -122,11 +177,15 @@ const MyOrderDetail = () => {
           <div className='payment-info'>
             <div className='flex justify-end  items-center py-3 text-right border-b border-b-[#ccc]'>
               <div className='text-[12px] pr-2'>Tổng tiền hàng</div>
-              <div className='w-[200px] text-[#866312] border-l border-l-[#ccc]'></div>
+              <div className='w-[200px] text-[#866312] border-l border-l-[#ccc]'>
+                {totalPrice && formatCurrency(totalPrice)}
+              </div>
             </div>
             <div className='flex justify-end  items-center py-3 text-right border-b border-b-[#ccc]'>
               <div className='text-[12px] pr-2'>Phí vận chuyển</div>
-              <div className='w-[200px] text-[#866312] border-l border-l-[#ccc]'></div>
+              <div className='w-[200px] text-[#866312] border-l border-l-[#ccc]'>
+                {orderData?.order?.priceShipping && formatCurrency(orderData?.order?.priceShipping)}
+              </div>
             </div>
             {/* <div className='flex justify-end  items-center py-3 text-right border-b border-b-[#ccc]'>
               <div className='text-[12px] pr-2'>Mã giảm giá</div>
@@ -134,16 +193,24 @@ const MyOrderDetail = () => {
             </div> */}
             <div className='flex justify-end  items-center py-3 text-right border-b border-b-[#ccc]'>
               <div className='text-[12px] pr-2'>Thành tiền</div>
-              <div className='w-[200px] text-2xl text-[#866312] border-l border-l-[#ccc]'></div>
+              <div className='w-[200px] text-2xl text-[#866312] border-l border-l-[#ccc]'>
+                {orderData?.order?.total && formatCurrency(orderData?.order?.total)}
+              </div>
             </div>
           </div>
           <div className='payment-method flex justify-end  items-center py-3 text-right'>
             <div className='flex items-center pr-2 gap-x-1'>
-              <RiMoneyDollarCircleFill className='text-[#866312] text-2xl' />
+              {orderData?.order?.paymentMethodId === 'cod' ? (
+                <RiMoneyDollarCircleFill className='text-[#866312] text-2xl' />
+              ) : (
+                <AiFillCreditCard className='text-[#866312]' />
+              )}
 
               <span className='text-[12px]'>Phương thức thanh toán</span>
             </div>
-            <div className='w-[200px] border-l border-l-[#ccc] text-sm text-[#EE4D2D]'>Thanh toán khi nhận hàng</div>
+            <div className='w-[200px] border-l border-l-[#ccc] text-sm text-[#EE4D2D]'>
+              {orderData?.order?.paymentMethodId === 'cod' ? 'Thanh toán khi nhận hàng' : 'Thanh toán qua VNPay'}
+            </div>
           </div>
         </div>
       </div>
